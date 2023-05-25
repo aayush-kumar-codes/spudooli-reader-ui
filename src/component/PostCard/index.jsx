382b6a0a3c28baed94b314acc794fbde08c1d18b
atul <atul.etech2011@gmail.com>
2023-05-25 11:54:06 +0530

    add-image

--- src/component/PostCard/index.jsx
+++ src/component/PostCard/index.jsx
@@ -5,6 +5,7 @@ import { MdOutlineUpload, MdOutlineHexagon } from 'react-icons/md'
 import { GiRotaryPhone } from 'react-icons/gi'
 import PostFooter from './PostFooter'
 import moment from 'moment'
+import matrixImage from '../../assets/images/matrix.jpg'
 
 function PostCard({feed}) {
 const inputDate = moment(feed?.date_published, 'ddd, DD MMM YYYY HH:mm:ss Z');
@@ -14,7 +15,7 @@ const outputDate = inputDate.format('MMMM D [at] h:mm A');
         <div className="flex-shrink-0">
           <img
             className="h-10 w-10 rounded-full"
-            src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            src={matrixImage}
             alt=""
           />
         </div>
